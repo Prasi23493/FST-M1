95085c740a957b051d3b6d5fe74376199418f647
Prasi23493 <97446090+Prasi23493@users.noreply.github.com>
2022-01-10 22:10:20 +0530

    Add files via upload

---
+++ BicycleParts.java
@@ -0,0 +1,11 @@
+package activities;
+
+public interface BicycleParts {
+    public int gears = 0;
+    public int currentSpeed = 0;
+}
+
+interface BicycleOperations {
+    public void applyBrake(int decrement);
+    public void speedUp(int increment);
+}
